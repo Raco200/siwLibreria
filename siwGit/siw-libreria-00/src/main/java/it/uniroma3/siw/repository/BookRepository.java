7d88bec715aa0a6a0ca4ab3cf370ea1b2721cde1
Raco200 <96876888+Raco200@users.noreply.github.com>
2023-05-18 10:38:14 +0200

    problema risolto ma ovviamente altri problemi

--- siwGit/siw-libreria-00/src/main/java/it/uniroma3/siw/repository/BookRepository.java
+++ siwGit/siw-libreria-00/src/main/java/it/uniroma3/siw/repository/BookRepository.java
@@ -1,7 +1,11 @@
 package it.uniroma3.siw.repository;
 import it.uniroma3.siw.model.Book;
+
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+
+
+
 public interface BookRepository extends CrudRepository<Book,Long> {
 	
 	public boolean existsBookByTitleAndISBN(String title,String isbn);
